65a5afcaab1470387cc4343ea343976f070dc84b
aeongdesu <osu@kakao.com>
2023-02-17 11:56:04 +0900

    AccountSwitcher: add toasts

--- AccountSwitcher/index.tsx
+++ AccountSwitcher/index.tsx
@@ -1,16 +1,20 @@
 import { Plugin } from "aliucord/entities";
+import { getAssetId } from "aliucord/utils";
 // @ts-ignore
 import { disablePlugin } from "aliucord/api"
-import { getByProps } from "aliucord/metro";
+// @ts-ignore
+import { Toasts, getByProps } from "aliucord/metro";
 
 export default class AccountSwitcher extends Plugin {
     public async start() {
         const config = getByProps("MultiAccountMobileExperiment").MultiAccountMobileExperiment.getCurrentConfig();
         if (!config.isMultiAccountMobileEnabled) {
             config.isMultiAccountMobileEnabled = true;
+            Toasts.open({ content: "AccountSwitcher is now enabled!"})
             disablePlugin("AccountSwitcher");
         } else {
-            this.logger.info("Already enabled AccountSwitcher experiment, stopping...")
+            this.logger.info("Already enabled AccountSwitcher experiment, disabling plugin...")
+            Toasts.open({ content: "Already enabled!", source: getAssetId("Small")})
             disablePlugin("AccountSwitcher");
         }
     }
