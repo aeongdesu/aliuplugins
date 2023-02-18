8ff1a1e7de60858a9b8fc10cdb98122d370ab257
aeongdesu <osu@kakao.com>
2023-02-18 13:06:34 +0900

    PluginDownload: make better

--- PluginDownloader/index.tsx
+++ PluginDownloader/index.tsx
@@ -1,15 +1,15 @@
 // like https://github.com/redstonekasi/vendetta-plugins/blob/main/plugins/url-import/src/index.js
 
+declare let aliucord: any
 import { Plugin } from "aliucord/entities";
 // @ts-ignore
 import { getByProps, Dialog, Toasts } from "aliucord/metro";
-// bad imports i think
-import { getAssetId } from "aliucord/utils";
-import { before } from "aliucord/utils/patcher";
+import { getAssetId } from "aliucord/utils"
 // @ts-ignore
-import { PLUGINS_DIRECTORY } from "aliucord/utils/constants";
+import { PLUGINS_DIRECTORY } from "aliucord/utils/constants"
+import { before } from "aliucord/utils/patcher"
 // @ts-ignore
-import { restartApp, fs } from "aliucord/native";
+import { fs } from "aliucord/native";
 
 const zip = new RegExp("https?://(?:github|raw\\.githubusercontent)\\.com/([A-Za-z0-9\\-_.]+)/([A-Za-z0-9\\-_.]+)/(?:raw|blob)?/?\\w+/builds/(\\w+).zip")
 
@@ -26,11 +26,33 @@ export default class PluginDownloader extends Plugin {
                 onPress: async () => {
                     const matches = url.match(zip);
                     try {
-                        await fs.download(url, `${PLUGINS_DIRECTORY}${matches[3]}.zip`);
+                        const installPlugin = async () => {
+                            await fs.download(url, `${PLUGINS_DIRECTORY}${matches[3]}.zip`);
+                            await aliucord.api.startPlugins()
+                            plugin = aliucord.api.plugins[matches[3]]
+                            if (plugin) return Toasts.open({ content: `Successfully installed ${plugin.manifest.name}!`, source: getAssetId("Check") })
+                            else {
+                                await fs.deleteFile(`${PLUGINS_DIRECTORY}${matches[3]}.zip`)
+                                return Toasts.open({ content: "Failed to install plugin!", source: getAssetId("Small") })
+                            }
+                        }
+                        let plugin = aliucord.api.plugins[matches[3]]
+                        if (plugin) {
+                            return Dialog.show({
+                                title: "Reinstall Plugin?",
+                                body: `${plugin.manifest.name} is already installed.`,
+                                confirmText: 'Yes',
+                                cancelText: 'No',
+                                isDismissable: true,
+                                onConfirm: () => installPlugin(),
+                            })
+                        }
+                        installPlugin()
                     } catch {
-                        return Toasts.open({ content: "Failed to download plugin!", source: getAssetId("Small") })
+                        await fs.deleteFile(`${PLUGINS_DIRECTORY}${matches[3]}.zip`)
+                        return Toasts.open({ content: "Failed to install plugin!", source: getAssetId("Small") })
                     }
-                    Dialog.show({
+                    /*Dialog.show({
                         title: "Reload required",
                         body: `Downloaded ${matches[3]}, reload Discord?\nexport loadPlugin when (never)`,
                         confirmText: 'Yes',
@@ -41,7 +63,7 @@ export default class PluginDownloader extends Plugin {
                             await fs.deleteFile(`${PLUGINS_DIRECTORY}${matches[3]}.zip`)
                             Toasts.open({ content: `${matches[3]}.zip has deleted.` })
                         }
-                    })
+                    })*/
                 }
             });
         });
