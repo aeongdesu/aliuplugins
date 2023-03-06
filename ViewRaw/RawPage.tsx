13f47ef59953f78c3bd9d5bb3ba1e2b667d74eb4
aeongdesu <osu@kakao.com>
2023-03-07 01:06:48 +0900

    rawpage

--- ViewRaw/RawPage.tsx
+++ ViewRaw/RawPage.tsx
@@ -10,18 +10,17 @@ const Button = getByProps("ButtonColors", "ButtonLooks", "ButtonSizes").default 
 export default function RawPage() {
     const message = JSON.stringify(ViewRaw.message, null, 4)
     return (<>
-        <ScrollView style={{ flex: 1 }}>
+        <ScrollView style={{ flex: 1, marginHorizontal: 13, marginVertical: 10 }}>
             <Button
                 text="Copy Raw Data"
                 color="brand"
                 size="small"
-                style={{ padding: 12 }}
                 onPress={() => {
                     Clipboard.setString(message)
                     Toasts.open({ content: "Copied data to clipboard", source: getAssetId("toast_copy_link") })
                 }}
             />
-            <Text selectable style={{ fontFamily: Constants.Fonts.CODE_SEMIBOLD, fontSize: 12, backgroundColor: "#282b30", color: "white", marginTop: 5, padding: 12 }}>
+            <Text selectable style={{ fontFamily: Constants.Fonts.CODE_SEMIBOLD, fontSize: 12, backgroundColor: "#282b30", color: "white", marginTop: 10, borderRadius: 3 }}>
                 {message}
             </Text>
         </ScrollView>
