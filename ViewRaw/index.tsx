1e0d5d68c3ab321eb38b69c1ba6754c788e18c18
aeongdesu <osu@kakao.com>
2023-03-26 11:22:08 +0900

    fix viewraw

--- ViewRaw/index.tsx
+++ ViewRaw/index.tsx
@@ -1,53 +1,54 @@
 import { Plugin } from "aliucord/entities"
 // @ts-ignore
-import { React, ReactNative, getByProps, Dialog, Toasts } from "aliucord/metro"
+import { React, getByProps, getByDisplayName, Forms } from "aliucord/metro"
 import { getAssetId } from "aliucord/utils"
 import RawPage from "./RawPage"
+
+const ActionSheet = getByProps("hideActionSheet")
+const Navigation = getByProps("push", "pushLazy", "pop")
+const DiscordNavigator = getByProps("getRenderCloseButton")
+const { default: Navigator, getRenderCloseButton } = DiscordNavigator
+const Icon = getByDisplayName("Icon")
+const { FormRow } = Forms
 
 export default class ViewRaw extends Plugin {
     static message: any
     public async start() {
-        const ActionSheet = getByProps("hideActionSheet")
-        const Navigation = getByProps("push", "pushLazy", "pop")
-        const DiscordNavigator = getByProps("getRenderCloseButton")
-        const { default: Navigator, getRenderCloseButton } = DiscordNavigator
-
         this.patcher.before(ActionSheet, "openLazy", (ctx) => {
             const [asyncComponent, args, actionMessage] = ctx.args
-            if (args == "MessageLongPressActionSheet")
-                asyncComponent.then(instance => {
-                    const unpatch = this.patcher.after(instance, "default", (_, component: any) => {
-                        const [msgProps, oldbuttons] = component.props?.children?.props?.children?.props?.children
-                        if (!msgProps) ViewRaw.message = actionMessage.message
-                        else ViewRaw.message = msgProps.props.message
-                        if (oldbuttons) {
-                            const MarkUnreadIndex = oldbuttons.findIndex((a: { props: { message: string } }) => a.props.message == "Copy Message Link")
-                            const ButtonRow = oldbuttons[MarkUnreadIndex].type
-                            const navigator = () => (
-                                <Navigator
-                                    initialRouteName="RawPage"
-                                    goBackOnBackPress={true}
-                                    screens={{
-                                        RawPage: {
-                                            title: "ViewRaw",
-                                            headerLeft: getRenderCloseButton(() => Navigation.pop()),
-                                            render: RawPage
-                                        }
-                                    }}
-                                />
-                            )
-                            component.props.children.props.children.props.children[1] = [...oldbuttons, <ButtonRow
-                                message="View Raw"
-                                iconSource={getAssetId("ic_chat_bubble_16px")}
-                                onPressRow={() => {
-                                    ActionSheet.hideActionSheet()
-                                    Navigation.push(navigator)
-                                }}
-                            />]
-                        }
-                        unpatch()
-                    })
+            if (args != "MessageLongPressActionSheet") return
+            asyncComponent.then(instance => {
+                const unpatch = this.patcher.after(instance, "default", (_, component: any) => {
+                    const [msgProps, oldbuttons] = component.props?.children?.props?.children?.props?.children
+                    if (!msgProps) ViewRaw.message = actionMessage.message
+                    else ViewRaw.message = msgProps.props.message
+                    if (!oldbuttons) return
+                    const navigator = () => (
+                        <Navigator
+                            initialRouteName="RawPage"
+                            goBackOnBackPress={true}
+                            screens={{
+                                RawPage: {
+                                    title: "ViewRaw",
+                                    headerLeft: getRenderCloseButton(() => Navigation.pop()),
+                                    render: RawPage
+                                }
+                            }}
+                        />
+                    )
+
+                    component.props.children.props.children.props.children[1] = [...oldbuttons,
+                    <FormRow
+                        label="View Raw"
+                        leading={<Icon source={getAssetId("ic_chat_bubble_16px")} />}
+                        onPress={() => {
+                            ActionSheet.hideActionSheet()
+                            Navigation.push(navigator)
+                        }}
+                    />]
+                    unpatch()
                 })
+            })
         })
     }
     public stop() {
